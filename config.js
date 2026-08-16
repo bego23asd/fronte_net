fba6b047ddfeb9d9c0ce3c9eb630fa44aee91087
bego23asd <greyburn2020@gmail.com>
2026-08-16 21:08:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-// ============================================================
-// ILAGAY DITO ang exact URL ng backend mo sa Render.
-// Halimbawa:  window.API_URL = "https://netflix-trial-backend.onrender.com";
-// (walang trailing slash)
-// ============================================================
-window.API_URL = "https://netflix-trial-backend-u2dh.onrender.com";
+
